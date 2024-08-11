954202fbd5610e6013fe5e6fe4bf750bea9a551b
k3nder <k3nder@outlook.es>
2024-08-11 12:41:27 +0200

    refactor(shaders): ensuring possible nulls

--- src/main/java/net/k3nder/gl/shader/Shaders.java
+++ src/main/java/net/k3nder/gl/shader/Shaders.java
@@ -3,17 +3,17 @@ package net.k3nder.gl.shader;
 import net.k3nder.test.Main;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Shaders {
     //public static final Shader SIMPLE = getShader("simple");
 
     public static Shader getDefaultShader(String name) {
         try {
-            Shader shader = Shader.create(
-                    Main.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".vert"),
-                    Main.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".frag")
+            return Shader.create(
+                    Objects.requireNonNull(Shader.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".vert")),
+                    Objects.requireNonNull(Shader.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".frag"))
                     );
-            return shader;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
