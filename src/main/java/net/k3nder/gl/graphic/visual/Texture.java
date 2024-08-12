ae5c8f74c97eeffc9af8ea8114d64a3391fc75ac
k3nder <k3nder@outlook.es>
2024-08-12 20:48:46 +0200

    prefi(texture): change texture loading system

--- src/main/java/net/k3nder/gl/graphic/visual/Texture.java
+++ src/main/java/net/k3nder/gl/graphic/visual/Texture.java
@@ -16,170 +16,93 @@ import java.util.HashMap;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.GL_RGB;
+import static org.lwjgl.opengl.GL12.glTexImage3D;
+import static org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER;
 import static org.lwjgl.opengl.GL13.glActiveTexture;
 import static org.lwjgl.opengl.GL30.glGenerateMipmap;
-import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.*;
 import static org.lwjgl.system.MemoryStack.stackPush;
 
 public class Texture {
-    private int id;
+    private final int id;
     private int width;
     private int height;
-    private Integer type;
-    private HashMap<Integer, Integer> configurations = new HashMap<>();
-    private InputStream image;
-    private Integer dChannels;
-    private Boolean flipV;
-    private Integer colorChannel;
 
-    private Texture() {}
-
-    private static Texture create(InputStream stream, int textureType, Integer dChannels,HashMap<Integer, Integer> configurations, boolean flipV, Integer colorChannel) {
-        Texture t = new Texture();
-
-        t.configurations = configurations;
-        t.type = textureType;
-        t.dChannels = dChannels;
-        t.image = stream;
-        t.flipV = flipV;
-        t.colorChannel = colorChannel;
-
-
-        return t;
+    public Texture() {
+        id = glGenTextures();
     }
-
-    public int getId() {
-        return id;
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
     }
+    public void setParameter(int param, int value) {
+        glTexParameteri(GL_TEXTURE_2D, param, value);
+    }
+    public void uploadData(int width, int height, ByteBuffer data) {
+        uploadData(GL_RGBA8, width, height, GL_RGBA, data);
+    }
+    public void uploadData(int internalFormat, int width, int height, int format, ByteBuffer data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    }
+    public void delete() {
+        glDeleteTextures(id);
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setWidth(int width) {
+        if (width > 0) {
+            this.width = width;
+        }
+    }
+    public void setHeight(int height) {
+        if (height > 0) {
+            this.height = height;
+        }
+    }
+    public static Texture createTexture(int width, int height, ByteBuffer data) {
+        Texture tex = new Texture();
+        tex.setWidth(width);
+        tex.setHeight(height);
 
-    public void init() {
-        try (MemoryStack stack = stackPush()) {
+        tex.bind();
 
+        tex.setParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+        tex.setParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        tex.setParameter(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        tex.setParameter(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        tex.uploadData(GL_RGBA8, width, height, GL_RGBA, data);
+
+        return tex;
+    }
+    public static Texture loadTexture(String path) {
+        ByteBuffer image;
+        int width, height;
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            /* Prepare image buffers */
             IntBuffer w = stack.mallocInt(1);
             IntBuffer h = stack.mallocInt(1);
-            IntBuffer channels = stack.mallocInt(1);
+            IntBuffer comp = stack.mallocInt(1);
 
-            ByteBuffer buffer = null;
-            buffer = readInputStreamToByteBuffer(image);
-            if (buffer == null)
-                throw new IOException("Failed to read texture buffer");
-
-            ByteBuffer data = STBImage.stbi_load_from_memory(buffer, w, h, channels, dChannels);
-
-            width = w.get(0);
-            height = h.get(0);
-
-            id = glGenTextures();
-            glBindTexture(type, id);
-
-            loadConfigurations(type);
-            STBImage.stbi_set_flip_vertically_on_load(flipV);
-
-
-            if (type == GL_TEXTURE_1D) {
-                glTexImage1D(type, 0, colorChannel, width, 0, colorChannel, GL_UNSIGNED_BYTE, data);
-            } else if (type == GL_TEXTURE_2D) {
-                glTexImage2D(type, 0, colorChannel, width, height, 0, colorChannel, GL_UNSIGNED_BYTE, data);
+            /* Load image */
+            stbi_set_flip_vertically_on_load(true);
+            image = stbi_load(path, w, h, comp, 4);
+            if (image == null) {
+                throw new RuntimeException("Failed to load a texture file!"
+                        + System.lineSeparator() + stbi_failure_reason());
             }
-            glGenerateMipmap(type);
 
-            stbi_image_free(data);
-        } catch (Exception t) {
-            t.printStackTrace();
+            /* Get width and height of image */
+            width = w.get();
+            height = h.get();
         }
+        return createTexture(width, height, image);
     }
 
-    private void loadConfigurations(int textureType) {
-        configurations.forEach((k, v) -> {
-            glTexParameteri(textureType, k, v);
-        });
+    public int getID() {
+        return id;
     }
-
-    public void addToShader(String key, Shader shader) {
-        shader.setI(key, this.id);
-    }
-
-    public void active(Integer texture) {
-        glActiveTexture(texture);
-        glBindTexture(this.type, this.id);
-    }
-
-    private static ByteBuffer readInputStreamToByteBuffer(InputStream inputStream) throws IOException {
-        if (inputStream == null) System.out.println("errs");
-        try (var readableByteChannel = Channels.newChannel(inputStream)) {
-            ByteBuffer buffer = BufferUtils.createByteBuffer(8192);
-            while (true) {
-                int bytesRead = readableByteChannel.read(buffer);
-                if (bytesRead == -1) break;
-                if (buffer.remaining() == 0) {
-                    ByteBuffer newBuffer = BufferUtils.createByteBuffer(buffer.capacity() * 2);
-                    buffer.flip();
-                    newBuffer.put(buffer);
-                    buffer = newBuffer;
-                }
-            }
-            buffer.flip();
-            return buffer;
-        }
-    }
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private HashMap<Integer, Integer> configurations = new HashMap<>();
-        private Integer type = GL_TEXTURE_2D;
-        private InputStream stream;
-        private Integer dChannels = 0;
-        private Integer colorChanel = GL_RGB;
-        private Boolean flipV = false;
-
-        public HashMap<Integer, Integer> getConfigurations() {
-            return configurations;
-        }
-
-        public Builder configuration(Integer key, Integer value) {
-            this.configurations.put(key, value);
-            return this;
-        }
-
-        public Builder type(int type) {
-            this.type = type;
-            return this;
-        }
-
-        public Builder source(InputStream stream) {
-            this.stream = stream;
-            return this;
-        }
-
-        public Builder dChannels(Integer dChannels) {
-            this.dChannels = dChannels;
-            return this;
-        }
-
-        public Builder source(String file) {
-            try {
-                this.stream = new FileInputStream(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            return this;
-        }
-
-        public Builder colorChanel(Integer colorChanel) {
-            this.colorChanel = colorChanel;
-            return this;
-        }
-
-        public Builder flipV() {
-            this.flipV = true;
-            return this;
-        }
-
-        public Texture create() {
-            return Texture.create(this.stream, this.type, this.dChannels, this.configurations, this.flipV, this.colorChanel);
-        }
-    }
-
 }
