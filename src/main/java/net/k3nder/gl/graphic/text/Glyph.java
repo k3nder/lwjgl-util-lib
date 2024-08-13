0e65ac7f3fe860ba34b27418e81282754d6c1d61
k3nder <k3nder@outlook.es>
2024-08-13 15:36:59 +0200

    rename FontLoader to Font

--- src/main/java/net/k3nder/gl/graphic/text/Glyph.java
+++ src/main/java/net/k3nder/gl/graphic/text/Glyph.java
@@ -1,14 +1,11 @@
 package net.k3nder.gl.graphic.text;
 
-import net.k3nder.gl.graphic.objects.Cube;
 import net.k3nder.gl.graphic.objects.ui.StaticCube;
-import net.k3nder.gl.graphic.shader.Shader;
-import net.k3nder.gl.graphic.visual.Texture;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class Glyph extends StaticCube {
-    public Glyph(FontLoader loader,char c, Vector2f pos) {
+    public Glyph(Font loader, char c, Vector2f pos) {
         super(new Vector3f(pos.x, pos.y, 0.0f), loader.getGlyphs().get(Character.valueOf(c)));
     }
 }
