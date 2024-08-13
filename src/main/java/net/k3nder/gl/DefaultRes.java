9c34294debf0230f15ee02fe070ed782107ce40d
k3nder <k3nder@outlook.es>
2024-08-13 15:36:36 +0200

    change Shaders to DefaultRes

---
+++ src/main/java/net/k3nder/gl/DefaultRes.java
@@ -0,0 +1,27 @@
+package net.k3nder.gl;
+
+import net.k3nder.gl.graphic.shader.Shader;
+import net.k3nder.gl.graphic.text.Font;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class DefaultRes {
+    public static Shader getShader(String name) {
+        try {
+            return Shader.create(
+                    Objects.requireNonNull(DefaultRes.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".vert")),
+                    Objects.requireNonNull(DefaultRes.class.getResourceAsStream("/default/shaders/" + name + "/" + name + ".frag"))
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Font getFont(String name, int size) {
+        try {
+            return new Font(Objects.requireNonNull(DefaultRes.class.getResourceAsStream("/default/fonts/" + name + ".ttf")), size);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
