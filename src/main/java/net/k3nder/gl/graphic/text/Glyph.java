4f6741d4aa4fc7e34412d3037b411e812ea47bb0
kristian <k3nder@outlook.es>
2024-08-15 18:46:48 +0200

    Update Glyph.java

--- src/main/java/net/k3nder/gl/graphic/text/Glyph.java
+++ src/main/java/net/k3nder/gl/graphic/text/Glyph.java
@@ -5,42 +5,93 @@ import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class Glyph extends GraphicalObject {
-     Polygon.builder()
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  ( 0.0f,  0.0f, -1.0f), new Vector2f( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f, -0.5f), new Vector3f ( 0.0f,  0.0f, -1.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f, -0.5f), new Vector3f ( 0.0f,  0.0f, -1.0f),  new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f, -0.5f), new Vector3f ( 0.0f,  0.0f, -1.0f),  new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f  ( 0.0f,  0.0f, -1.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  ( 0.0f,  0.0f, -1.0f), new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f  ( 0.0f,  0.0f, 1.0f),  new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f,  0.5f), new Vector3f ( 0.0f,  0.0f, 1.0f),   new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 0.0f,  0.0f, 1.0f),   new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 0.0f,  0.0f, 1.0f),   new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f  ( 0.0f,  0.0f, 1.0f),  new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f  ( 0.0f,  0.0f, 1.0f),  new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f  (-1.0f,  0.0f,  0.0f), new Vector2f ( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f, -0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f, -0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f, -0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f,  0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 1.0f,  0.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  ( 0.0f, -1.0f,  0.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f, -0.5f), new Vector3f ( 0.0f, -1.0f,  0.0f),  new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f,  0.5f), new Vector3f ( 0.0f, -1.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f, -0.5f,  0.5f), new Vector3f ( 0.0f, -1.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f,  0.5f),new Vector3f  ( 0.0f, -1.0f,  0.0f), new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f, -0.5f, -0.5f),new Vector3f  ( 0.0f, -1.0f,  0.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f  ( 0.0f,  1.0f,  0.0f), new Vector2f ( 0.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f, -0.5f), new Vector3f ( 0.0f,  1.0f,  0.0f),  new Vector2f( 1.0f, 1.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 0.0f,  1.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(0.5f,  0.5f,  0.5f), new Vector3f ( 0.0f,  1.0f,  0.0f),  new Vector2f( 1.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f,  0.5f),new Vector3f  ( 0.0f,  1.0f,  0.0f), new Vector2f ( 0.0f, 0.0f)))
-            .vertex(Vertex.create(new Vector3f(-0.5f,  0.5f, -0.5f),new Vector3f (  0.0f,  1.0f,  0.0f), new Vector2f(  0.0f, 1.0f)))
-
+      private Shader shader;
+    private static final Polygon MODEL =  Polygon.builder()
+            .put(-0.5f).put( -0.5f).put( -0.5f).put( 0.0f).put(0.0f).put( -1.0f).put(0.0f).put( 0.0f)
+            .put(0.5f ).put(-0.5f ).put(-0.5f).put( 0.0f).put(  0.0f).put( -1.0f).put( 1.0f).put( 0.0f)
+            .put(0.5f ).put( 0.5f ).put(-0.5f).put( 0.0f).put( 0.0f).put( -1.0f).put(1.0f).put( 1.0f)
+            .put(0.5f ).put( 0.5f ).put(-0.5f).put( 0.0f).put( 0.0f).put( -1.0f).put( 1.0f).put( 1.0f)
+            .put(-0.5f).put(  0.5f).put( -0.5f).put(0.0f).put(  0.0f).put( -1.0f).put( 0.0f).put( 1.0f)
+            .put(-0.5f).put( -0.5f).put( -0.5f).put( 0.0f).put(  0.0f).put( -1.0f).put( 0.0f).put( 0.0f)
+            .put(-0.5f).put( -0.5f).put(  0.5f).put( 0.0f).put(  0.0f).put( 1.0f).put( 0.0f).put( 0.0f)
+            .put(0.5f).put(-0.5f).put( 0.5f).put( 0.0f).put(0.0f).put( 1.0f).put( 1.0f).put( 0.0f)
+            .put(0.5f).put( 0.5f).put( 0.5f).put( 0.0f).put(0.0f).put( 1.0f).put(  1.0f).put( 1.0f)
+            .put(0.5f).put( 0.5f).put( 0.5f).put( 0.0f).put(  0.0f).put( 1.0f).put(  1.0f).put( 1.0f)
+            .put(-0.5f).put(  0.5f).put(  0.5f).put ( 0.0f).put(  0.0f).put( 1.0f).put(  0.0f).put( 1.0f)
+            .put(-0.5f).put( -0.5f).put(  0.5f).put ( 0.0f).put(  0.0f).put( 1.0f).put(  0.0f).put( 0.0f)
+            .put(-0.5f).put(  0.5f).put(  0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(-0.5f).put(  0.5f).put( -0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 1.0f)
+            .put(-0.5f).put( -0.5f).put( -0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(-0.5f).put( -0.5f).put( -0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(-0.5f).put( -0.5f).put(  0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 0.0f)
+            .put(-0.5f).put(  0.5f).put(  0.5f).put (-1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(0.5f ).put( 0.5f ).put( 0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(0.5f ).put( 0.5f ).put(-0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 1.0f)
+            .put(0.5f ).put(-0.5f ).put(-0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(0.5f ).put(-0.5f ).put(-0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(0.5f ).put(-0.5f ).put( 0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  0.0f).put( 0.0f)
+            .put(0.5f ).put( 0.5f ).put( 0.5f).put( 1.0f).put(  0.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(-0.5f).put( -0.5f).put( -0.5f).put ( 0.0f).put( -1.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(0.5f ).put(-0.5f ).put(-0.5f).put( 0.0f).put( -1.0f).put(  0.0f).put(  1.0f).put( 1.0f)
+            .put(0.5f ).put(-0.5f ).put( 0.5f).put( 0.0f).put( -1.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(0.5f ).put(-0.5f ).put( 0.5f).put( 0.0f).put( -1.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(-0.5f).put( -0.5f).put(  0.5f).put ( 0.0f).put( -1.0f).put(  0.0f).put(  0.0f).put( 0.0f)
+            .put(-0.5f).put( -0.5f).put( -0.5f).put ( 0.0f).put( -1.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(-0.5f).put(  0.5f).put( -0.5f).put ( 0.0f).put(  1.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .put(0.5f ).put( 0.5f).put(-0.5f).put( 0.0f).put(  1.0f).put(  0.0f).put(  1.0f).put( 1.0f)
+            .put(0.5f).put( 0.5f ).put( 0.5f).put( 0.0f).put(  1.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(0.5f).put( 0.5f ).put( 0.5f).put( 0.0f).put(  1.0f).put(  0.0f).put(  1.0f).put( 0.0f)
+            .put(-0.5f).put(  0.5f).put(  0.5f).put ( 0.0f).put(  1.0f).put(  0.0f).put(  0.0f).put( 0.0f)
+            .put(-0.5f).put(  0.5f).put( -0.5f).put(  0.0f).put(  1.0f).put(  0.0f).put(  0.0f).put( 1.0f)
+            .build();
+    public Glyph(Font loader, char c, Vector2f pos) {
+        super();
+        texture = loader.getGlyphs().get(c);
+        model.translate(new Vector3f(pos.x, pos.y, 0));
+        shader = Shader.create(
+                """
+                #version 330
+                
+                layout (location = 0) in vec3 aPos;
+                layout (location = 1) in vec3 aNormal;
+                layout (location = 2) in vec2 aTexCoords;
+                
+                uniform mat4 model;
+                uniform mat4 view;
+                uniform mat4 projection;
+                
+                out vec3 Normal;
+                out vec2 TexCoords;
+                out vec3 FragPos;
+                
+                void main() {
+                    gl_Position = model * vec4(aPos, 1.0);
+                    FragPos = vec3(model * vec4(aPos, 1.0));
+                    Normal = mat3(transpose(inverse(model))) * aNormal;
+                    TexCoords = vec2(aTexCoords.x, 1.0 - aTexCoords.y);
+                }
+                """,
+                """
+                #version 330
+                
+                out vec4 FragColor;
+                
+                in vec2 TexCoords;
+                
+                uniform sampler2D tex;
+                
+                void main() {
+                    FragColor = texture(tex, TexCoords);
+                }
+                """);
+    }
+    @Override
+    public void render(Shader shader) {
+        this.shader.use();
+        super.render(this.shader);
+    }
+    @Override
+    public void load() {
+        polygon = MODEL;
+    }
 }
